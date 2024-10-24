55947594ed2e499010cfc03a5d3d45e128ab3ec4
xiaoxin <36990855+rainmaker22@users.noreply.github.com>
2024-10-24 14:18:58 +0800

    Update install_pyg.sh

--- scripts/install_pyg.sh
+++ scripts/install_pyg.sh
@@ -1,6 +1,10 @@
 mkdir pyg_depend && cd pyg_depend
-wget https://data.pyg.org/whl/torch-1.12.0%2Bcu102/torch_cluster-1.6.0%2Bpt112cu102-cp38-cp38-linux_x86_64.whl
-wget https://data.pyg.org/whl/torch-1.12.0%2Bcu102/torch_scatter-2.1.0%2Bpt112cu102-cp38-cp38-linux_x86_64.whl
-python3 -m pip install torch_cluster-1.6.0+pt112cu102-cp38-cp38-linux_x86_64.whl
-python3 -m pip install torch_scatter-2.1.0+pt112cu102-cp38-cp38-linux_x86_64.whl
+wget https://data.pyg.org/whl/torch-1.12.0%2Bcu113/torch_cluster-1.6.0%2Bpt112cu113-cp39-cp39-linux_x86_64.whl
+wget https://data.pyg.org/whl/torch-1.12.0%2Bcu113/torch_scatter-2.1.0%2Bpt112cu113-cp39-cp39-linux_x86_64.whl
+wget https://data.pyg.org/whl/torch-1.12.0%2Bcu113/torch_sparse-0.6.16%2Bpt112cu113-cp39-cp39-linux_x86_64.whl
+wget https://data.pyg.org/whl/torch-1.12.0%2Bcu113/torch_spline_conv-1.2.1%2Bpt112cu113-cp39-cp39-linux_x86_64.whl
+python3 -m pip install torch_cluster-1.6.0+pt112cu113-cp39-cp39-linux_x86_64.whl
+python3 -m pip install torch_scatter-2.1.0+pt112cu113-cp39-cp39-linux_x86_64.whl
+python3 -m pip install torch_sparse-0.6.16+pt112cu113-cp39-cp39-linux_x86_64.whl
+python3 -m pip install torch_spline_conv-1.2.1+pt112cu113-cp39-cp39-linux_x86_64.whl
 python3 -m pip install torch_geometric
